d360743cca1c940186cbb05b9f951081ac639756
Franklyne-kibet <rootkibet@gmail.com>
2023-01-14 23:57:40 +0300

    change of dir

--- week_1_basics_n_setup/2_docker_sql/setup.sh
+++ week_1_basics_n_setup/2_docker_sql/setup.sh
@@ -31,7 +31,7 @@ docker run -it \
     -e POSTGRES_USER="root" \
     -e POSTGRES_PASSWORD="root" \
     -e POSTGRES_DB="ny_taxi" \
-    -v c:/Users/user/Documents/de/2_docker_sql/ny_taxi_postgres_data:/var/lib/postgresql/data\
+    -v c:/Users/user/Documents/data-zoomcamp/week_1_basics_n_setup/2_docker_sql/ny_taxi_postgres_data:/var/lib/postgresql/data\
     -p 5432:5432 \
     --network=pg-network \
     --name pg-database \
